46ef8872e0c7df57239fb688574c76cad5c7f523
Benjamin Müller <bm@fork.ch>
2014-04-26 12:35:09 +0200

    adopted path

--- tools/deploy_to_raspberry_pi.sh
+++ tools/deploy_to_raspberry_pi.sh
@@ -2,5 +2,7 @@
 
 ./gradlew clean assemble
 FILE=AdHoc-Railway/build/apk/AdHoc-Railway-debug-unaligned.apk
+ssh baehnle@adhocserver 'rm -rf /var/www/adhoc-railway/artifacts-android && mkdir /var/www/adhoc-railway/artifacts-android'
+
 scp $FILE baehnle@adhocserver:~/AdHoc-Railway/
-scp $FILE baehnle@adhocserver:/var/www/adhoc-railway/
+scp $FILE baehnle@adhocserver:/var/www/adhoc-railway/artifacts-android
